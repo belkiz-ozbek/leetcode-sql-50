ef885313ae09819c9cefcd0b66b4189f778f7fb3
Belkız Özbek <74767722+belkiz-ozbek@users.noreply.github.com>
2025-12-11 14:17:36 +0300

    Time: 542 ms (52.53%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,17 +1,31 @@
 # Write your MySQL query statement below
+
+#another solution
 SELECT ROUND(
         COUNT(*) / (SELECT COUNT(DISTINCT player_id)
                                     FROM Activity), 2) AS fraction
 FROM Activity a2
 INNER JOIN (
-        SELECT a.player_id, a.event_date
-        FROM Activity a
-        INNER JOIN (SELECT player_id, MIN(event_date) AS initial_date
+        SELECT player_id, MIN(event_date) AS event_date
         FROM Activity
         GROUP BY player_id
-    ) m ON a.player_id = m.player_id AND a.event_date = m.initial_date
 ) d ON a2.player_id = d.player_id  
     AND a2.event_date != d.event_date
-WHERE ABS(DATEDIFF(a2.event_date, d.event_date)) = 1
+WHERE DATEDIFF(a2.event_date, d.event_date) = 1
 
+
+#SELECT ROUND(
+#        COUNT(*) / (SELECT COUNT(DISTINCT player_id)
+#                                    FROM Activity), 2) AS fraction
+#FROM Activity a2
+#INNER JOIN (
+#        SELECT a.player_id, a.event_date
+#        FROM Activity a
+#        INNER JOIN (SELECT player_id, MIN(event_date) AS initial_date
+#        FROM Activity
+#        GROUP BY player_id
+#    ) m ON a.player_id = m.player_id AND a.event_date = m.initial_date
+#) d ON a2.player_id = d.player_id  
+#    AND a2.event_date != d.event_date
+#WHERE DATEDIFF(a2.event_date, d.event_date) = 1
 
